8d7c8ad4b7e70a2b98959d6495b0ad704248bb9c
Marino Di Clemente <kernelfolla@gmail.com>
2014-11-10 17:15:32 -0500

    minor fix

--- Utils/ExportHelper.php
+++ Utils/ExportHelper.php
@@ -8,39 +8,54 @@ class ExportHelper
 {
     const INDEX_ALPHA     = true;
     const DEFAULT_FORMAT  = 'xls';
-    const THROW_EXCEPTION = false;
+    const THROW_EXCEPTION = true;
 
     private static $writers = array(
         'xls' => 'Excel5',
-        'csv' => 'CSV'
+        'csv' => 'CSV',
+        'html' => 'HTML',
+        'pdf' => 'PDF',
+        'xlsx' => 'Excel2007'
     );
     private static $mimes = array(
         'xls' => 'application/vnd.ms-excel',
-        'csv' => 'text/csv'
+        'csv' => 'text/csv',
+        'html' => 'text/html',
+        'pdf' => 'application/pdf',
+        'xlsx' => 'application/vnd.ms-excel'
     );
 
-    protected static $headers = array();
-    protected static $fields = array();
+    protected $headers = array();
+    protected $fields = array();
 
-    public function execute($source, $format = static::DEFAULT_FORMAT, $filename = null)
+    protected $accessor;
+
+
+    public function execute($source, $format = null, $filename = null)
     {
+
+        $this->accessor = PropertyAccess::createPropertyAccessor();
+        if(!isset($format)){
+            $format = static::DEFAULT_FORMAT;
+        }
         $filename = $this->processFilename($filename, $format);
         $obj = $this->createObject();
-        $this->writeHeader($format, $filename);
         $this->fillRows($source, $obj);
         $objWriter = \PHPExcel_IOFactory::createWriter($obj, $this->getWriterName($format));
+
+        $this->writeHeader($format, $filename);
         $objWriter->save('php://output');
         exit;
     }
     protected function processFilename($filename, $format = null){
         if ($filename == null) {
-            $filename = 'export{DATE}{TIME}.' . $format;
+            $filename = 'export{DATE}-{TIME}.' . $format;
         }
-        if(strpos($filename),'{DATE}'){
-            $filename = str_replace('{DATE}', date('Y-m-d'));
+        if(strpos($filename,'{DATE}') !== false){
+            $filename = str_replace('{DATE}', date('Y-m-d'), $filename);
         }
-        if(strpos($filename),'{TIME}'){
-            $filename = str_replace('{TIME}', date('H:i:s'));
+        if(strpos($filename,'{TIME}') !== false){
+            $filename = str_replace('{TIME}', date('H-i-s'), $filename);
         }
  
         return $filename;    
@@ -52,10 +67,10 @@ class ExportHelper
     {
         $obj = new \PHPExcel();
         $obj->setActiveSheetIndex(0);
-        $headers = static::$headers;
+        $headers = $this->headers;
 
         if (empty($headers)) {
-            $headers = static::$fields;
+            $headers = $this->fields;
         }
         if (static::INDEX_ALPHA) {
             $headers = StringUtils::indexNumAlpha($headers);
@@ -74,14 +89,14 @@ class ExportHelper
     protected function fillRows($source, \PHPExcel $obj)
     {
         $rowCount = 2;
-        $accessor = PropertyAccess::createPropertyAccessor();
-        $fields   = static::$fields;
+        $fields   = $this->fields;
         if (static::INDEX_ALPHA) {
             $fields = StringUtils::indexNumAlpha($fields);
         }
+
         foreach ($source as $entity) {
             foreach ($fields as $k => $v) {
-               $val = (string)$val;
+                $val = $this->getValue($entity, $v);
                 $obj->getActiveSheet()->SetCellValue($k . $rowCount, $val);
             }
             $rowCount++;
@@ -89,25 +104,28 @@ class ExportHelper
     }
 
     protected function getValue($entity, $key){
-        try {
-            $val = $accessor->getValue($entity, $key);
-        } catch (\Exception $e) {
-            $val = '';
+        if(StringUtils::startsWith($key,'@')){
+            $val = $entity;
+        }else{
+            try {
+                $val = $this->accessor->getValue($entity, $key);
+            } catch (\Exception $e) {
+                $val = '';
+            }
         }
-        return $this->processVal($val, $key); 
+        return $this->processValue($val, $key); 
     }
 
-    protected function processval($val, $key){
+    protected function processValue($val, $key){
         if ($val instanceof \DateTime) {
             $val = $val->format('d/m/Y h:i');
         }
-        return $val;
+        return (string) $val;
     }
 
     protected function writeHeader($format, $filename)
     {
         header('Content-Type: ' . self::$mimes[$format]);
-        header("Content-type: text/csv");
         header('Content-Disposition: attachment;filename="' . $filename . '"');
         header('Cache-Control: max-age=0');
     }
